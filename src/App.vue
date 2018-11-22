99f1e05f41566cbaac97576108cefb0774688227
Matthew Levy <matn53@gmail.com>
2018-11-22 22:38:44 +0000

    change primary nav layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar toggleable="md" type="dark" variant="dark">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand><router-link to="/">Home</router-link></b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item href="/">Home</b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
     <b-container id="app">
       <router-view/>
     </b-container>
+    <footer>
+
+    </footer>
   </div>
 </template>
 
@@ -18,5 +23,10 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+footer {
+  background-color: #2c3e50;
+  min-height: 200px;
+  margin-top: 20px;
 }
 </style>
