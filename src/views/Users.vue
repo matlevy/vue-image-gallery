2136c54444155c8378140348a5efb273034df148
Matthew Levy <matn53@gmail.com>
2018-11-22 21:23:25 +0000

    user list on homepage

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -6,11 +6,34 @@
         <div v-if="error">
             {{ error }}
         </div>
-        <div v-if="users">
-            {{ users }}
+        <div v-if="users" class="gallery-grid">
+            <b-card 
+                :title="user.name"
+                v-for="(user, index) in users" v-bind:key="index"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="m-2">
+                <p class="card-text">
+                    {{user.email}}
+                </p>
+                <router-link :to="{ name: 'user', params: { id: user.id }}"><b-button variant="primary">View</b-button></router-link>
+            </b-card>
         </div>
     </div>
 </template>
+
+<style lang="scss">
+    .gallery-grid {
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        justify-content: center;
+    }
+</style>
+
 
 <script>
 import {
