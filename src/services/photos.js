b58ed10c3d7777f3846f90b2862e1ae7a0248929
Matthew Levy <matn53@gmail.com>
2018-11-22 20:17:41 +0000

    update to service promises

--- src/services/photos.js
+++ src/services/photos.js
@@ -1,22 +1,28 @@
 class PhotoService {
     constructor(){}
     getAllPhotos() {
-        return fetch(`https://jsonplaceholder.typicode.com/photos`);
+        return fetch(`https://jsonplaceholder.typicode.com/photos`)
+            .then(response => response.json());
     }
     getPhoto(id) {
-        return fetch(`https://jsonplaceholder.typicode.com/photos/${id}`);
+        return fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
+            .then(response => response.json());
     }
     getAlbums() {
-        return fetch(`https://jsonplaceholder.typicode.com/albums`);
+        return fetch(`https://jsonplaceholder.typicode.com/albums`)
+            .then(response => response.json());
     }
     getAlbumsByUser(userId) {
         return fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`)
+            .then(response => response.json())
     }
     getAlbumById(id) {
         return fetch(`https://jsonplaceholder.typicode.com/albums/${id}`)
+            .then(response => response.json())
     }
     getAlbumPhotos(albumId) {
         return fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+            .then(response => response.json())
     }
 }
 
--- src/services/users.js
+++ src/services/users.js
@@ -1,7 +1,12 @@
 export class UserService {
     constructor(){}
     getAll(){
-        return fetch('https://jsonplaceholder.typicode.com/users');
+        return fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json());
+    }
+    getUser(userId) {
+        return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+            .then(response => response.json());
     }
 }
 
