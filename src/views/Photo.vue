7c4de5f4306a5656a4a30179ed3e6681a24a903d
Matthew Levy <matn53@gmail.com>
2018-11-23 00:44:48 +0000

    photo view

--- src/views/Photo.vue
+++ src/views/Photo.vue
@@ -6,11 +6,23 @@
         <div v-if="error">
             {{ error }}
         </div>
-        <div v-if="photo">
-            {{ photo }}
+        <div v-if="photo" class="photo">
+            <b-img :src="photo.url" fluid :alt="photo.title" />
+            <h1 class="photo-title">{{ photo.title }}</h1>
+            <b-button target="_blank" :href="photo.url" variant="primary" class="mt-3 mr-3">Download</b-button>
+            <router-link :to="{ name: 'user-photos', params: { gallery: photo.albumId }}"><b-button variant="primary" class="mt-3 mr-3">View Gallery</b-button></router-link>
         </div>
     </div>
 </template>
+<style lang="scss">
+    .photo {
+        text-align: left;
+    }
+    .photo-title {
+        margin-top: 1em;
+    }
+</style>
+
 <script>
 import {
     photoService
@@ -36,7 +48,7 @@ export default {
       fetch() {
           this.error = this.photo = null;
           this.loading = true;
-          photoService.getPhoto(this.$route.params.id).then(
+          photoService.getPhoto(this.$route.params.photoId).then(
               (data) => {
                   this.photo = data;
                   this.loading = false;
